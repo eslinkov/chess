b34b825b31c03f4b4f383d3a5f0c9deeef9a8332
Emma <emma.slinkov@gmail.com>
2026-04-01 19:23:43 -0600

    logout command, create game command

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -28,7 +28,10 @@ public class ChessClient {
             }
 
             String line = scanner.nextLine().trim();
-            String[] user_inputs = line.split(" ");
+            if (line.isEmpty()) {
+                continue;
+            }
+            String[] user_inputs = line.split("\\s+");
             String command = user_inputs[0].toLowerCase();
 
             if (authToken == null) {
@@ -40,7 +43,16 @@ public class ChessClient {
                     default -> System.out.println("Unknown command. Type 'help for options.'");
                 }
             } else {
-                // postlogin commands
+                switch (command) {
+                    case "help" -> System.out.println("Postlogin commands: create, list, join, observe, logout, quit, help");
+                    case "quit" -> result = "quit";
+                    case "create" -> create(user_inputs);
+//                    case "list"
+//                    case "join"
+//                    case "observe"
+                    case "logout" -> logout();
+                    default -> System.out.println("Unknown command. Type 'help for options.'");
+                }
 
             }
 
@@ -54,7 +66,7 @@ public class ChessClient {
     private void register(String[] user_inputs) {
         try {
             if (user_inputs.length != 4) {
-                System.out.println("Expected: register <username>, <password>, <email>");
+                System.out.println("Expected: register <username> <password> <email>");
                 return;
             }
             var result = server.register(user_inputs[1], user_inputs[2], user_inputs[3]);
@@ -66,13 +78,45 @@ public class ChessClient {
 
     }
 
-    private void login(String[] user_inputs) throws ResponseException {
-        String username = user_inputs[1];
-        String password = user_inputs[2];
+    private void login(String[] user_inputs) {
+        try {
+            if (user_inputs.length != 3) {
+                System.out.println("Expected: login <username> <password>");
+                return;
+            }
+            var result = server.login(user_inputs[1], user_inputs[2]);
+            authToken = result.authToken();
+            System.out.println("Welcome back " + result.username() + "!");
+        } catch (ResponseException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void logout() {
+        try {
+            server.logout(authToken);
+            authToken = null;
+            System.out.println("Successfully logged out.");
+        } catch (ResponseException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 
+    private void create(String[] user_inputs) {
+        try {
+            if (user_inputs.length != 2) {
+                System.out.println("Expected: create <NAME>");
+                return;
+            }
+            var result = server.createGame(user_inputs[1], authToken);
+            System.out.println("Created game: " + user_inputs[1] + " gameID: " + result.gameID());
+        } catch (ResponseException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
 
+// register testuser1 password test@email.com
 
 }
