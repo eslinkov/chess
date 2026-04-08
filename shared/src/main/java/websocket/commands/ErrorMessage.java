6a559c36a1e0107581b34f6be09537147ca100ec
Emma <emma.slinkov@gmail.com>
2026-04-07 21:10:54 -0600

    add WebSocket message classes

---
+++ shared/src/main/java/websocket/commands/ErrorMessage.java
@@ -0,0 +1,16 @@
+package websocket.commands;
+
+import websocket.messages.ServerMessage;
+
+public class ErrorMessage extends ServerMessage {
+    private final String errorMessage;
+
+    public ErrorMessage(String errorMessage) {
+        super(ServerMessageType.ERROR);
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
